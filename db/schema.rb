4bbca51da5ad628f4599464c1193095fcca4f730
kpagcha <pabloantonio.garciachacon@alum.uca.es>
2014-04-21 17:22:34 +0200

    Error menor corregido

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140407222602) do
+ActiveRecord::Schema.define(:version => 20140418175006) do
 
   create_table "brands", :force => true do |t|
     t.string   "name",                    :null => false

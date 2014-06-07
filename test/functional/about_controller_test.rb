4eaee513dc5d1b3cb63cd4a39e23399842fd76b5
Salcarman <salcarman@gmail.com>
2014-06-07 19:47:33 +0200

    Tests mejorados.

--- test/functional/about_controller_test.rb
+++ test/functional/about_controller_test.rb
@@ -1,9 +1,13 @@
+# encoding: utf-8
 require 'test_helper'
 
 class AboutControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_template 'about/index'
+    assert_equal 'Sobre Caña de Lomo', assigns(:page_title)
+    assert_tag 'title', :content => 'Sobre Caña de Lomo'
   end
 
 end
--- test/functional/catalog_controller_test.rb
+++ test/functional/catalog_controller_test.rb
@@ -1,10 +1,11 @@
+# encoding: utf-8
 require 'test_helper'
 
 class CatalogControllerTest < ActionController::TestCase
   fixtures :suppliers, :brands, :products
 
   setup do
-    @brand = brands(:brand1)
+    @product = products(:product1)
     @controller = CatalogController.new
     @request = ActionController::TestRequest.new
     @response = ActionController::TestResponse.new
@@ -13,21 +14,48 @@ class CatalogControllerTest < ActionController::TestCase
   test "index" do
     get :index
     assert_response :success
+    assert_tag 'h1', :content => 'Catálogo de productos'
+    assert_tag 'p', :content => 'Últimos productos'
+    @products = Product.find :all, :order => "products.id desc"
+    pages = @products.count / 10
+    pages += 1 unless @products.count % 10 == 0
+    cont = 1
+    page = 1
+    @products.each do |a|
+      assert_tag :tag => 'dt', :content => a.name
+      cont = cont + 1
+      if cont > 10 && page < pages  # go to next page
+        page = page + 1
+        cont = 1
+        get :index, :page => page
+        assert_response :success
+      end
+    end
   end
 
   test "show" do
-    get :show, :id => @brand
+    get :show, :id => 1
     assert_response :success
+    assert_template 'catalog/show'
+    assert_equal 'Producto 1', assigns(:product).name
+    assert_equal 'Descripcion producto 1', assigns(:product).description
+    assert_tag 'h1', :content => Product.find(1).name
   end
 
   test "search" do
     get :search
     assert_response :success
+    assert_tag 'h1', :content => 'Buscar producto'
   end
 
   test "latest" do
     get :latest
     assert_response :success
+    assert_tag 'h1', :content => 'Últimos productos'
+    @products = Product.latest 10
+    @products.each do |a|
+      assert_tag :tag => 'dt', :content => a.name
+    end
   end
 
 end
